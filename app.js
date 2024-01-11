713a2e4bf478bae938e89e310117c6060a84b380
Zhi Wang <jerrywang65@hotmail.com>
2024-01-10 22:32:52 -0800

    Campground Index

--- app.js
+++ app.js
@@ -19,18 +19,10 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({
-        title: 'My Backyard', description: 'cheap camping!'
-    });
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
 });
-
-// app.get('/campgrounds', async (req, res) => {
-//     const campgrounds = await Campground.find({});
-//     res.render('campgrounds/index', { campgrounds });
-// });
 
 app.listen(PORT, () => {
     console.log('listening on port 3000');
